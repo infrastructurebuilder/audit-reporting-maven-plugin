6767d78b68d44536bb315c78025b868dd4af7500
Sparrow Estes <sparrowestes@gmail.com>
2021-08-11 18:00:11 -0500

    changed to .stream(), try w resources

--- src/main/java/org/infrastructurebuilder/audit/excel/ExcelGenerator.java
+++ src/main/java/org/infrastructurebuilder/audit/excel/ExcelGenerator.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2019 admin (admin@infrastructurebuilder.org)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,77 +20,64 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
-import org.infrastructurebuilder.audit.auditor.model.AuditResult;
 import org.infrastructurebuilder.audit.auditor.model.AuditorInputSource;
-import org.infrastructurebuilder.audit.auditor.model.AuditorResults;
 import org.infrastructurebuilder.audit.auditor.model.io.xpp3.AuditorResultsModelXpp3ReaderEx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.Objects.requireNonNull;
 
 public final class ExcelGenerator {
-
   private final static Logger log = LoggerFactory.getLogger(ExcelGenerator.class);
 
   public static void generate(Path input, Path output) throws IOException, XmlPullParserException {
 
-    XSSFWorkbook workbook = new XSSFWorkbook();
-
-    List<AuditorResults> results;
-    try (InputStream in = java.nio.file.Files.newInputStream(input)) {
-      results = new AuditorResultsModelXpp3ReaderEx().read(in, true, new AuditorInputSource()).getAudits();
-    }
-    requireNonNull(results);
-    log.info("Generating excel file");
-
-    /*
-     * sheets
-     */
-    results.stream().forEach(resultsSet -> {
-      String sheetName = resultsSet.getName();
-      XSSFSheet sheet = workbook.createSheet(sheetName);
+    try (InputStream in = java.nio.file.Files.newInputStream(input);
+         XSSFWorkbook workbook = new XSSFWorkbook()) {
+      log.info("Generating excel file");
 
       /*
-       * headers
+       * sheets
        */
-      List<String> headers = new ArrayList<>();
-      headers.addAll(resultsSet.getDescriptionHeaders());
-      int currentHeaderCellNumber = 0;
-      Row headerRow = sheet.createRow(0);
-      for (String header : headers) {
-        Cell cell = headerRow.createCell(currentHeaderCellNumber++);
-        cell.setCellValue(header);
-      }
+      requireNonNull(new AuditorResultsModelXpp3ReaderEx().read(in, true, new AuditorInputSource()).getAudits()).stream().forEach(resultsSet -> {
+        XSSFSheet sheet = workbook.createSheet(resultsSet.getName());
 
-      /*
-       * descriptions
-       */
-      int currentDescriptionRowNumber = 1;
-      for (AuditResult result : resultsSet.getResults()) {
-        Row descriptionRow = sheet.createRow(currentDescriptionRowNumber);
-        int currentDescriptionCellNumber = 0;
-        for (String description : result.getDescriptions()) {
-          Cell cell = descriptionRow.createCell(currentDescriptionCellNumber);
-          cell.setCellValue(description);
-          currentDescriptionCellNumber++;
-        }
-        currentDescriptionRowNumber++;
-      }
-    });
+        /*
+         * headers
+         */
+        AtomicInteger currentHeaderCellNumber = new AtomicInteger(0);
+        Row headerRow = sheet.createRow(0);
+        resultsSet.getDescriptionHeaders().stream().forEach(header -> {
+          Cell cell = headerRow.createCell(currentHeaderCellNumber.getAndIncrement());
+          cell.setCellValue(header);
+        });
 
-    try (FileOutputStream out = new FileOutputStream(String.valueOf(output))) {
-      log.info("Writing excel file");
-      workbook.write(out);
-      workbook.close();
-      log.info("Excel file written successfully!");
+        /*
+         * descriptions
+         */
+        AtomicInteger currentDescriptionRowNumber = new AtomicInteger(1);
+        resultsSet.getResults().stream().forEach(result -> {
+          Row descriptionRow = sheet.createRow(currentDescriptionRowNumber.getAndIncrement());
+          AtomicInteger currentDescriptionCellNumber = new AtomicInteger(0);
+          result.getDescriptions().stream().forEach(description -> {
+            Cell cell = descriptionRow.createCell(currentDescriptionCellNumber.getAndIncrement());
+            cell.setCellValue(description);
+          });
+        });
+      });
+
+      try (OutputStream out = Files.newOutputStream(output)) {
+        log.info("Writing excel file");
+        workbook.write(out);
+        log.info("Excel file written successfully!");
+      }
     }
   }
 }
